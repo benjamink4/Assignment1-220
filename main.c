aded549aa136d771dc065115ebcb32e796023104
benjamink4 <41266777+benjamink4@users.noreply.github.com>
2020-02-07 11:25:36 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+char* print_any_base(unsigned long num, unsigned int base){
+
+}
+
+unsigned int count_vowels(char *str){
+
+}
+char *interleave(char *s1, char *s2){
+
+}
+unsigned char *prime_vector(unsigned long num){
+
+}
+char *reverse_words(char *str){
+
+}
+int main() {
+    printf("Hello, World!\n");
+    return
+}
